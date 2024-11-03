67a1f0687c6c9aca968ab96c5a4c2e6bcb59fd25
Manuel Anzola <136187762+manzola016@users.noreply.github.com>
2024-11-03 16:35:53 -0400

    Delete Ejercicio Java 3 directory

--- Ejercicio Java 3/src/main/java/com/example/crudjava/repository/EstudianteRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.example.crudjava.repository;
-
-import com.example.crudjava.entity.Estudiante;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EstudianteRepositorio extends JpaRepository<Estudiante, Long> {
-}
