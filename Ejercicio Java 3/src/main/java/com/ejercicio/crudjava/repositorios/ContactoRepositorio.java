f0a5f49afaabe173005312435cfed3eb47a1ec06
Manuel Anzola <136187762+manzola016@users.noreply.github.com>
2024-11-12 21:19:22 -0400

    Delete Ejercicio Java 3 directory

--- Ejercicio Java 3/src/main/java/com/ejercicio/crudjava/repositorios/ContactoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.ejercicio.crudjava.repositorios;
-
-import com.ejercicio.crudjava.domain.entities.ContactoEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactoRepositorio extends CrudRepository<ContactoEntity, Long> {
-}
